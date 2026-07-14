b854b9a41677699bbe6d8488bc12cfc13c79d1e2
Dhathri128 <dhathrikaranam5@gmail.com>
2026-07-14 07:48:27 +0530

    Added array demo

---
+++ arrays/arraydemo.java
@@ -0,0 +1,8 @@
+public class arraydemo{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,5,4,6};
+        for(int num : arr){
+            System.out.println(num);
+        }
+    }
+}

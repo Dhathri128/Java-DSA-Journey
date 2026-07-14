7df69dca59a3c2103402493c536192c8d40632c2
Dhathri128 <dhathrikaranam5@gmail.com>
2026-07-14 07:58:36 +0530

    Added maxElement program

---
+++ arrays/maxElement.java
@@ -0,0 +1,16 @@
+
+public class maxElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 4, 6};
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum element = " + max);
+    }
+}

30d696362ce862b2940d2608e635c1f00a222a29
Dhathri128 <dhathrikaranam5@gmail.com>
2026-08-31 06:23:15 +0530

    Practiced couple of Problems like primecheck, Fiboancci, Factorial,Reverse an Integer

---
+++ Basic/PrimeCheck.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class PrimeCheck {
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n <= 1) {
+            System.out.println("Not Prime");
+            return;
+        }
+
+        boolean isPrime = true;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+    }*/
+
+        public static void main(String[] args) {
+            int n = 57;
+            if(n<1){
+                System.out.println("Not Prime");
+            }
+            for(int i=2;i*i<=n;i++){
+                if(n%i==0){
+                    System.out.println("Not Prime");
+                    return;
+                }
+            }
+            System.out.println("Prime");
+        }
+}
